e0185d97363d2247f202c237047331dec425382c
marcelduplessis <marceldpl10@gmail.com>
2024-07-24 10:13:07 +0200

    figure 5 code

---
+++ code/functions/detrend_dim.py
@@ -0,0 +1,9 @@
+import xarray as xr
+
+# detrend the sst_max anomalies
+
+def detrend_dim(da, dim, deg=1):
+    # detrend along a single dimension
+    p = da.polyfit(dim=dim, deg=deg)
+    fit = xr.polyval(da[dim], p.polyfit_coefficients)
+    return da - fit
