757de6b4ed6ee8d81b3696bd5759a4fe43bf3571
marcelduplessis <marceldpl10@gmail.com>
2024-12-24 12:01:48 +0100

    feature: storm classification

--- code/functions/download_era5_cdsapi.py
+++ code/functions/download_era5_cdsapi.py
@@ -3,11 +3,11 @@ from urllib.request import urlopen# start the client
 
 def download_era5_cdsapi(var, days, month, year, dataset="reanalysis-era5-single-levels", product_type='reanalysis', path=''):
     
-    cds = cdsapi.Client()# dataset you want to read
+    client = cdsapi.Client()# dataset you want to read
 
     if dataset=='reanalysis-era5-single-levels-monthly-means':
 
-        params = {
+        request = {
                   'variable': [var],
                   'product_type': product_type,
                   'year': [year],
@@ -19,17 +19,28 @@ def download_era5_cdsapi(var, days, month, year, dataset="reanalysis-era5-single
                            '21:00', '22:00', '23:00'
                           ],
                   'area': ['0', '0', '-90', '360'],
-                  'format': 'netcdf'
+                  'data_format': 'netcdf'
                  }
 
     else: 
         
-        params = {
+        request = {
                   'variable': [var],
-                  'product_type': product_type,
+                  'product_type': [product_type],
                   'year': [year],
                   'month': [month],
-                  'day': days,
+                  'day': [
+                      "01", "02", "03",
+                      "04", "05", "06",
+                      "07", "08", "09",
+                      "10", "11", "12",
+                      "13", "14", "15",
+                      "16", "17", "18",
+                      "19", "20", "21",
+                      "22", "23", "24",
+                      "25", "26", "27",
+                      "28", "29", "30",
+                      "31"],
                   'time': [
                            '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
                            '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00',
@@ -39,11 +50,15 @@ def download_era5_cdsapi(var, days, month, year, dataset="reanalysis-era5-single
                   'area': [
                            '0', '0', '-90', '360'
                           ],
-                  'format': 'netcdf'
+                  'data_format': 'netcdf'
                  }
+        
+    print('downloading: ', var, year, month)
+
+    target = path+str(var)+'_'+str(year)+str(month)+'.nc'
     
-    fl = cds.retrieve(dataset, params) # download the file 
+    fl = client.retrieve(dataset, request, target) # download the file 
     
-    fl.download(path+str(var)+'_'+str(year)+str(month)+'.nc') # load into memory
+    # fl.download(path+str(var)+'_'+str(year)+str(month)+'.nc') # load into memory
         
     return
